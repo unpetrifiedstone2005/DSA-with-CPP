1c4c84231f054ddc0f6b5d74d7ad762170b2cd5f
unpetrifiedstone2005 <arya.duttagupta1@gmail.com>
2024-10-06 00:49:06 +0530

    Leetcode 567

--- HashMaps#1.cpp
+++ HashMaps#1.cpp
@@ -1,4 +1,5 @@
-/*Given two strings s1 and s2, return true if s2 contains a 
+/*
+Given two strings s1 and s2, return true if s2 contains a 
 permutation
  of s1, or false otherwise.
 
@@ -20,7 +21,8 @@ Output: false
 Constraints:
 
 1 <= s1.length, s2.length <= 104
-s1 and s2 consist of lowercase English letters.*/
+s1 and s2 consist of lowercase English letters.
+*/
 
 
 
